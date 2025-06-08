63ac4111e376354d830b3a422f7f369f6a3119ab
Takeru Ohta <phjgt308@gmail.com>
2025-06-08 22:41:51 +0900

    Add Chunker::new() method and introduce generic ChunkedFile and Chunk structs

--- src/chunker.rs
+++ src/chunker.rs
@@ -1,4 +1,4 @@
-use std::num::NonZeroUsize;
+use std::{num::NonZeroUsize, path::PathBuf, time::Duration};
 
 #[derive(Debug)]
 pub struct Chunker {
@@ -6,5 +6,25 @@ pub struct Chunker {
     pub step_size: NonZeroUsize,
 }
 
+impl Chunker {
+    pub fn new() -> Self {
+        Self {
+            window_size: NonZeroUsize::MIN.saturating_add(99),
+            step_size: NonZeroUsize::MIN.saturating_add(49),
+        }
+    }
+}
+
 #[derive(Debug)]
-pub struct Chunk {}
+pub struct ChunkedFile<T> {
+    pub path: PathBuf,
+    pub time: Duration,
+    pub labels: Vec<String>,
+    pub chunks: Vec<Chunk<T>>,
+}
+
+#[derive(Debug)]
+pub struct Chunk<T> {
+    pub line: NonZeroUsize,
+    pub data: T,
+}
