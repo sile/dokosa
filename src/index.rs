9f28b0114a0b877be8013dca3400f84038ed899c
Takeru Ohta <phjgt308@gmail.com>
2025-06-09 20:30:24 +0900

    Add index data structures for repositories and chunked files with embedding support

--- src/index.rs
+++ src/index.rs
@@ -1,15 +1,39 @@
+use std::{
+    collections::BTreeMap,
+    path::{Path, PathBuf},
+};
+
+use crate::{chunker::Chunk, embedder::Embedding};
+
 #[derive(Debug)]
-pub struct IndexFile {}
+pub struct IndexFile {
+    path: PathBuf,
+    repositories: BTreeMap<PathBuf, IndexedRepository>,
+}
 
-// #[derive(Debug)]
-// pub struct Repository {
-//     pub path: PathBuf,
-//     pub commit: String,
-//     pub files: ChunkedFile,
-// }
+impl IndexFile {
+    pub fn load_or_create<P: AsRef<Path>>(path: P) -> orfail::Result<Self> {
+        let path = path.as_ref().to_path_buf();
+        if path.exists() {
+            todo!()
+        } else {
+            Ok(Self {
+                path,
+                repositories: BTreeMap::new(),
+            })
+        }
+    }
+}
 
-// #[derive(Debug)]
-// pub struct ChunkedFile {
-//     pub path: PathBuf,
-//     pub chunks: Vec<Chunk<Embedding>>,
-// }
+#[derive(Debug)]
+pub struct IndexedRepository {
+    pub path: PathBuf,
+    pub commit: String,
+    pub files: ChunkedFile,
+}
+
+#[derive(Debug)]
+pub struct ChunkedFile {
+    pub path: PathBuf,
+    pub chunks: Vec<Chunk<Embedding>>,
+}
