367563c870cdc055d98f94983b67df4011bad56d
Takeru Ohta <phjgt308@gmail.com>
2025-06-09 09:41:55 +0900

    Add command-line arguments for repository path and index file configuration

--- src/subcommand_index.rs
+++ src/subcommand_index.rs
@@ -1,10 +1,19 @@
-use std::io::Read;
+use std::{io::Read, path::PathBuf};
 
 use orfail::OrFail;
 
 use crate::chunker::Chunker;
 
-pub fn run(args: noargs::RawArgs) -> noargs::Result<()> {
+pub fn run(mut args: noargs::RawArgs) -> noargs::Result<()> {
+    let repository_path: PathBuf = noargs::arg("GIT_REPOSITORY_PATH")
+        .example("/path/to/git/repository/")
+        .take(&mut args)
+        .then(|a| a.value().parse())?;
+    let index_path: PathBuf = noargs::opt("index-file")
+        .env("SAGURU_INDEX_FILE")
+        .default(".saguru")
+        .take(&mut args)
+        .then(|a| a.value().parse())?;
     if let Some(help) = args.finish()? {
         print!("{help}");
         return Ok(());
