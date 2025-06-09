f34c534f8cf5ec8bee930674714ded60256dfa38
Takeru Ohta <phjgt308@gmail.com>
2025-06-09 20:41:58 +0900

    Add JSON serialization/deserialization support for ChunkedFile struct

--- src/index.rs
+++ src/index.rs
@@ -37,3 +37,24 @@ pub struct ChunkedFile {
     pub path: PathBuf,
     pub chunks: Vec<Chunk<Embedding>>,
 }
+
+impl nojson::DisplayJson for ChunkedFile {
+    fn fmt(&self, f: &mut nojson::JsonFormatter<'_, '_>) -> std::fmt::Result {
+        f.object(|f| {
+            f.member("path", self.path.to_string_lossy().as_ref())?;
+            f.member("chunks", &self.chunks)
+        })
+    }
+}
+
+impl<'text> nojson::FromRawJsonValue<'text> for ChunkedFile {
+    fn from_raw_json_value(
+        value: nojson::RawJsonValue<'text, '_>,
+    ) -> Result<Self, nojson::JsonParseError> {
+        let ([path, chunks], []) = value.to_fixed_object(["path", "chunks"], [])?;
+        Ok(ChunkedFile {
+            path: path.try_to()?,
+            chunks: chunks.try_to()?,
+        })
+    }
+}
