f7df508e5f7ba4a2f7087737feeefa50249159c2
Takeru Ohta <phjgt308@gmail.com>
2025-06-11 18:40:01 +0900

    Add comment listing available subcommands

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() -> noargs::Result<()> {
     }
     noargs::HELP_FLAG.take_help(&mut args);
 
+    // add, remove, update, list, search
     if noargs::cmd("embed").take(&mut args).is_present() {
         dokosa::subcommand_embed::run(args)?;
     } else if noargs::cmd("chunk").take(&mut args).is_present() {
