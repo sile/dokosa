b3c8d4667e9db4d2a726e871efb5211b4e09667c
Takeru Ohta <phjgt308@gmail.com>
2025-06-12 19:56:24 +0900

    Add JSON serialization support and change embedding type from Vec<f64> to Embedding in index structures

--- src/index_file.rs
+++ src/index_file.rs
@@ -2,6 +2,8 @@ use std::path::{Path, PathBuf};
 
 use orfail::OrFail;
 
+use crate::embedder::Embedding;
+
 #[derive(Debug)]
 pub struct IndexFile {
     pub path: PathBuf,
@@ -40,9 +42,54 @@ pub struct RepositoryEntry {
     pub commit: String,
 }
 
+impl nojson::DisplayJson for RepositoryEntry {
+    fn fmt(&self, f: &mut nojson::JsonFormatter<'_, '_>) -> std::fmt::Result {
+        f.object(|f| {
+            f.member("path", &self.path)?;
+            f.member("commit", &self.commit)
+        })
+    }
+}
+
+impl<'text> nojson::FromRawJsonValue<'text> for RepositoryEntry {
+    fn from_raw_json_value(
+        value: nojson::RawJsonValue<'text, '_>,
+    ) -> Result<Self, nojson::JsonParseError> {
+        let ([path, commit], []) = value.to_fixed_object(["path", "commit"], [])?;
+        Ok(Self {
+            path: path.try_to()?,
+            commit: commit.try_to()?,
+        })
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct ChunkEntry {
     pub path: PathBuf,
     pub line: usize,
-    pub embedding: Vec<f64>,
+    pub embedding: Embedding,
+}
+
+impl nojson::DisplayJson for ChunkEntry {
+    fn fmt(&self, f: &mut nojson::JsonFormatter<'_, '_>) -> std::fmt::Result {
+        f.object(|f| {
+            f.member("path", &self.path)?;
+            f.member("line", self.line)?;
+            f.member("embedding", &self.embedding)
+        })
+    }
+}
+
+impl<'text> nojson::FromRawJsonValue<'text> for ChunkEntry {
+    fn from_raw_json_value(
+        value: nojson::RawJsonValue<'text, '_>,
+    ) -> Result<Self, nojson::JsonParseError> {
+        let ([path, line, embedding], []) =
+            value.to_fixed_object(["path", "line", "embedding"], [])?;
+        Ok(Self {
+            path: path.try_to()?,
+            line: line.try_to()?,
+            embedding: embedding.try_to()?,
+        })
+    }
 }
