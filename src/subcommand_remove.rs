fdeec225da95d35d847e24e3c9965f55280dd610
Takeru Ohta <phjgt308@gmail.com>
2025-06-14 21:06:48 +0900

    Implement repository and chunk removal logic in remove subcommand

--- src/subcommand_remove.rs
+++ src/subcommand_remove.rs
@@ -2,7 +2,10 @@ use std::path::PathBuf;
 
 use orfail::OrFail;
 
-use crate::{git::GitRepository, index_file::IndexFile};
+use crate::{
+    git::GitRepository,
+    index_file::{IndexFile, IndexFileEntry},
+};
 
 pub fn run(mut args: noargs::RawArgs) -> noargs::Result<()> {
     let repo_path: PathBuf = noargs::arg("GIT_REPOSITORY_PATH")
@@ -34,6 +37,30 @@ pub fn run(mut args: noargs::RawArgs) -> noargs::Result<()> {
     if dry_run {
         return Ok(());
     }
+
+    let temp_index_file =
+        IndexFile::create_new(index_file.path.with_extension("temp")).or_fail()?;
+    let mut removing = false;
+    for entry in index_file.entries() {
+        let entry = entry.or_fail()?;
+        match entry {
+            IndexFileEntry::Repository(x) => {
+                if x.path == repo.root_dir {
+                    removing = true;
+                } else {
+                    removing = false;
+                    index_file.append_repository(&x).or_fail()?;
+                }
+            }
+            IndexFileEntry::Chunk(x) => {
+                if removing {
+                    continue;
+                }
+                index_file.append_chunk(&x).or_fail()?;
+            }
+        }
+    }
+    std::fs::rename(temp_index_file.path, index_file.path).or_fail()?;
 
     eprintln!("=> Removed");
     Ok(())
